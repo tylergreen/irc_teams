a4e6855563c785e745e74243c0cbe7bc5ce2c9a0
Tyler Green <tyler.green2@gmail.com>
2013-07-25 18:57:18 -0500

    added team class and tests

--- lib/irc_teams/team.rb
+++ lib/irc_teams/team.rb
@@ -1,3 +1,11 @@
-class Team
+require 'set'
+
+class Team < Set
+  attr_reader :name
+
+  def initialize(name)
+    super([])
+    @name = name
+  end
   
 end
--- test/test_team.rb
+++ test/test_team.rb
@@ -2,7 +2,23 @@ require "test/unit"
 require 'irc_teams/team'
 
 describe Team do
-  it 'has tests' do
-    assert_equal(5, 1+4)
+  it 'has a name' do
+    t = Team.new('psg')
+    assert_equal(t.name, 'psg')
+  end
+    
+  it 'can add a player' do
+    t = Team.new('test team')
+    t.add('Thiago')
+    assert_includes(t, 'Thiago')
+  end
+
+  it 'can remove a player' do
+    t = Team.new('psg')
+    t.add('Thiago')
+    t.add('Ibra')
+    t.delete('Thiago')
+    assert(t.include?('Ibra'))
+    assert(not(t.include?('Thiago')))
   end
 end
