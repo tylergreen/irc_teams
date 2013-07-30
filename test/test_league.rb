210e840103df64541a2226adcbd4045f91b936b5
Tyler Green <tyler.green2@gmail.com>
2013-07-29 22:49:03 -0500

    added more tests

--- test/test_league.rb
+++ test/test_league.rb
@@ -1,36 +1,39 @@
 require "test/unit"
-require 'irc_teams/team_list'
+require 'irc_teams/league'
 
-describe TeamList do
+describe League do
   before do
-    @list = TeamList.new
-    @list.create_team('Napoli')
+    @league = League.new
+    @league.create_team('Napoli')
   end
 
   it 'can create a new team' do
-    assert(@list.teams['Juventus'].nil?)
-    @list.create_team('Juventus')
-    assert(@list.teams['Juventus'])
+    @league.create_team('Juventus')
+    assert(@league.find_team('Juventus'))
   end
 
-  it 'can add someone to a team' do
-    assert( @list.add_player('Gervinho', 'Napoli'))
-    assert_includes(@list.teams['Napoli'], 'Gervinho')
+  it 'will not create an existing team' do
+    assert_raises(LeagueError) { @league.create_team('Napoli') }
   end
 
   it 'can remove someone from a team' do
-    assert( @list.remove_player('Gervinho', 'Napoli'))
-    assert(not(@list.find_team('Napoli').include?('Gervinho')))
+    assert( @league.remove_player('Gervinho', 'Napoli'))
+    assert(not(@league.find_team('Napoli').include?('Gervinho')))
   end
 
   it 'can find what team a player is on' do
-    assert( @list.add_player('Gervinho', 'Napoli'))
-    assert_equal('Napoli', @list.find_team_by_player('Gervinho').name)
+    assert( @league.add_player('Gervinho', 'Napoli'))
+    assert_equal('Napoli', @league.find_team_by_player('Gervinho').name)
+  end
+
+  it 'will not add a player to a team that it is already on' do
+    assert( @league.add_player('Gervinho', 'Napoli'))
+    assert_raises(LeagueError) { @league.add_player('Gervinho', 'Napoli') }
   end
 
   it 'can remove a team' do
-    assert(@list.find_team('Napoli'))
-    @list.remove_team('Napoli')
-    assert_raises(RuntimeError) { @list.find_team('Napoli') } 
+    assert(@league.find_team('Napoli'))
+    @league.remove_team('Napoli')
+    assert_raises(NonExistentTeamError) { @league.find_team('Napoli') } 
   end
 end
