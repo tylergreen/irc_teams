a69d70b246b73db7dcaebbcd35d55c7ea082f9c2
Tyler Green <tyler.green2@gmail.com>
2013-07-30 13:13:36 -0500

    added capture groups to commands

--- lib/irc_teams/commands.rb
+++ lib/irc_teams/commands.rb
@@ -1,10 +1,10 @@
 module Commands
-  CreateTeam = /create team \S+/
-  JoinTeam = /join \S+/
+  CreateTeam = /create team (\S+)/
+  JoinTeam = /join (\S+)/
   LeaveTeam = /leave team/
   ShowAllTeams = /show teams/
-  ShowTeam = /show team \S+/
+  ShowTeam = /show team (\S+)/
   ShowMyTeam = /show my team/
-  DeleteTeam = /delete team \S+/
+  DeleteTeam = /delete team (\S+)/
   Reset = /reset/
 end
